98c4668dcc2b5f492c137d591621a362d2920988
ansh123456788 <46483577+ansh123456788@users.noreply.github.com>
2019-01-12 23:23:57 +0530

    Add files via upload

---
+++ DigitSep.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class DigitSep
+/*Separate the digits and place 3 spaces in betweens*/
+{
+    public static void main(String[] args)
+    {
+     int a,b,c,d,e,f;
+     Scanner s = new Scanner(System.in);
+     System.out.println("Enter a five digit number");
+     a = s.nextInt();
+     b = a%10;
+     a = a/10;
+     c = a%10;
+     a = a/10;
+     d = a%10;
+     a = a/10;
+     e = a%10;
+     a = a/10;
+     f = a%10;
+     a = a/10;
+     if(a==0)
+     {
+     	System.out.println("your output is");
+     	System.out.print(f+"   "+e+"   "+d+"   "+c+"   "+b);
+     }
+    }
+
+}
